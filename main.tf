086def5b1323fc27f846d84f7af0b9a93fbc8801
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-21 15:39:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,13 @@ resource "aws_subnet" "public_subnet_1" {
    Name = "Public Subnet 1"
  }
 }
+
+resource "aws_subnet" "public_subnet_2" {
+ vpc_id            = aws_vpc.ot_microservices_dev.id
+ cidr_block        = "10.0.0.16/28"
+ availability_zone = "us-east-2b"
+ map_public_ip_on_launch = true
+ tags = {
+   Name = "Public Subnet 2"
+ }
+}
