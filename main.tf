c7f3585594752dcc8c1bf74344eb290c6ef0c8d9
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-21 15:36:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ resource "aws_vpc" "ot_microservices_dev" {
     Name = "ot-micro-vpc"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+ vpc_id            = aws_vpc.ot_microservices_dev.id
+ cidr_block        = "10.0.0.0/28"
+ availability_zone = "us-east-2a"
+ map_public_ip_on_launch = true
+ tags = {
+   Name = "Public Subnet 1"
+ }
+}
